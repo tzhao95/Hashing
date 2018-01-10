698558c40f1bcf503f7064c644c9d07d0ce82008
tzhao95 <tzhao95@gmail.com>
2018-01-10 00:57:24 -0500

    multi file for python hash

--- Hash.py
+++ Hash.py
@@ -1,4 +1,8 @@
+'''
 hashTable = [0]*10 #intialized a 10 index array for hashtable
+
+#initalize a 10index array of arrays in case multiple values have same key
+hashTable2 = [[] for _ in range(10)] 
 
 print(hashTable)
 
@@ -13,7 +17,29 @@ def hashFunct(string):
 def insert(table, key, value):
 	table[hashFunct(key)] = value
 
+def insert2(table, key, value):
+	table[hashFunct(key)].append(value)
 
 print(hashFunct("hello"))
 insert(hashTable, "hello", 24601)
 print(hashTable)
+print(hashTable2)
+insert2(hashTable2, "hello", 24601)
+print(hashTable2)
+insert2(hashTable2, "hello", 10029)
+print(hashTable2)
+'''
+class Hash():
+
+	def __init__(self, size):
+		self.size = size
+		self.list = [[]for _ in range(size)]
+
+	def hashFunct(self, string):
+		hash_value = 0
+		for c in string:
+			hash_value = hash_value + ord(c)
+		return hash_value%len(self.list)
+
+	def insert(self, key, value):
+		self.list[hashFunct(key)].append(value)
---
+++ main.py
@@ -0,0 +1,4 @@
+from Hash import Hash
+
+s = Hash(10)
+print(s)
